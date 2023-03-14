0c2729afe5304a69ee543e7ee4562e9704695604
unknown <User@partner.3new.com>
2023-03-14 15:53:32 +0800

    关闭重复的窗口

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,11 @@
 import * as vscode from "vscode";
+import { WebviewPanel } from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "test-plugin.helloWorld",
-    () => {
-      const panel = vscode.window.createWebviewPanel(
+  let panel: WebviewPanel | undefined;
+  const showContent = () => {
+    if (panel === undefined) {
+      panel = vscode.window.createWebviewPanel(
         "firstTool", //标识webview的类型。在内部使用
         "firstTool", //显示给用户的面板标题
         vscode.ViewColumn.One, //编辑器列中显示新的webview面板
@@ -15,12 +16,18 @@ export function activate(context: vscode.ExtensionContext) {
       );
       // add content
       panel.webview.html = getWebviewContent();
+    } else {
+      panel.reveal(); // 调用reveal()或拖动一个web视图面板到一个新的编辑器列移动web视图到那个新列。
     }
-  );
 
-  context.subscriptions.push(disposable);
+    return panel;
+  };
+  context.subscriptions.push(
+    vscode.commands.registerCommand("test-plugin.helloWorld", showContent),
+    vscode.commands.registerCommand("test-plugin.helloWorld1", showContent)
+  );
 }
-export function deactivate() {}
+// export function deactivate() {}
 
 const getWebviewContent = () => {
   return `<!DOCTYPE html>
